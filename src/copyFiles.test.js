0b7e37a2c315f428b8a5f38c4be49f85317df256
Gabriel Duarte <gabrielqduarte@gmail.com>
2018-01-20 19:32:21 -0200

    Added tests to copyFiles

---
+++ src/copyFiles.test.js
@@ -0,0 +1,34 @@
+import copyFile from "./copyFiles"
+import overwriteComponentNameMock from "./overwriteComponentName"
+
+jest.mock("./overwriteComponentName")
+
+describe("copyFile", () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it("should call overwriteComponentName with the correct set of arguments", () => {
+    const filesRenamed = ["NewApp.js"]
+    const files = ["App.js"]
+    const componentNameOriginal = "App"
+    const newComponentName = "NewApp"
+    copyFile(filesRenamed, files, componentNameOriginal, newComponentName)
+    expect(overwriteComponentNameMock).toHaveBeenCalledTimes(1)
+    expect(overwriteComponentNameMock).toHaveBeenCalledWith(
+      files[0],
+      filesRenamed[0],
+      componentNameOriginal,
+      newComponentName,
+    )
+  })
+
+  it("should call overwriteComponentName 4 times if there is 4 entries in filesRenamed", () => {
+    const filesRenamed = ["NewApp.js", "NewApp.js", "NewApp.js", "NewApp.js"]
+    const files = ["App.js", "App.js", "App.js", "App.js"]
+    const componentNameOriginal = "App"
+    const newComponentName = "NewApp"
+    copyFile(filesRenamed, files, componentNameOriginal, newComponentName)
+    expect(overwriteComponentNameMock).toHaveBeenCalledTimes(4)
+  })
+})
