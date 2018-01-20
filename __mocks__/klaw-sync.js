fc9c75cff3534e8874696e6e0a6260b42fef643d
Gabriel Duarte <gabrielqduarte@gmail.com>
2018-01-20 18:00:17 -0200

    added tests to listFilesInsideDir

---
+++ __mocks__/klaw-sync.js
@@ -0,0 +1,11 @@
+const files = [
+  { path: "src/components/App.js" },
+  { path: "src/components/App.test.js" },
+  { path: "src/components/App.md" },
+  { path: "src/components/App.css" },
+  { path: "src/components/index" },
+]
+
+const klawSync = jest.fn(() => files)
+
+module.exports = klawSync
