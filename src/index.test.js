b0e3e6e0c4f35bcefe8b8490e747bd46253d3852
Gabriel Duarte <gabrielqduarte@gmail.com>
2018-01-28 06:37:52 -0200

    test(index): added coverage to the index file
    Verify that the expected api functions are indeed being exported

---
+++ src/index.test.js
@@ -0,0 +1,13 @@
+import * as index from "./index"
+
+describe("index", () => {
+  it("should export the componentFinder function", () => {
+    expect(index.componentFinder).toBeDefined()
+  })
+  it("should export the copyPasteComponent function", () => {
+    expect(index.copyPasteComponent).toBeDefined()
+  })
+  it("should export the getDefaultComponentPath function", () => {
+    expect(index.getDefaultComponentPath).toBeDefined()
+  })
+})
